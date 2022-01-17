c84e35c059ea4e56558910a6b7648eb113318504
Scheronimus <97701193+Scheronimus@users.noreply.github.com>
2022-01-17 22:48:56 +0100

    Add controller and some actions for the menu

---
+++ src/main/java/org/scheronimus/palette_editor/PrototypeController.java
@@ -0,0 +1,26 @@
+package org.scheronimus.palette_editor;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class PrototypeController {
+
+	// Event Listener on MenuItem[#aboutMenu].onAction
+	@FXML
+	public void printAbout(ActionEvent event) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("About Palette Editor");
+		alert.setHeaderText(null);
+		alert.setContentText("Palette Editor\nVersion xx\nCopytight (C) 2022 Scheronimus");
+
+		alert.showAndWait();
+	}
+
+	@FXML
+	public void closeApplication(ActionEvent event) {
+		Platform.exit();
+	}
+}
